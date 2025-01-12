77306be1839fed770cc077839d8f94d8a9821efb
Gabriel Pacheco <gblpacheco@gmail.com>
2025-01-12 16:23:31 -0300

    JS
    Implementando JS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Proteína - 400gr por pessoa, se passar de 6 horas o valor sobe pra 650g por pessoa.
+
+// Cerveja - 1200ml por pessoa, se passar de 6 horas o valor sobe pra 2000ml por pessoa.
+
+// Refrigerante e água - 1000ml por pessoa, se passar de 6 horas o valor sobe pra 1500ml por pessoa.
+
+// Crianças valem 0,5 pessoas.
+
+let r = document.getElementById("res")
+r.innerText = `Olá Mundo`
