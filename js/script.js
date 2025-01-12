e33996554a7944db3bccef9cb11eca8e79803bb8
Gabriel Pacheco <gblpacheco@gmail.com>
2025-01-12 17:38:31 -0300

    Upgrade
    Atualização do JS

--- js/script.js
+++ js/script.js
@@ -6,5 +6,31 @@
 
 // Crianças valem 0,5 pessoas.
 
-let r = document.getElementById("res")
-r.innerText = `Olá Mundo`
+
+function calcular () {
+    
+    const transformUnd = 1000;
+
+    const proteina = 400;
+    const proteinaKid = 200;
+    const beer = 600;
+    const nonAlcoholic = 500;
+    const nonAlcoholicKids = 250;
+    
+    let adult = parseInt(document.getElementById("adult").value);
+    let kids = parseInt(document.getElementById("kids").value);
+    let duration = parseInt(document.getElementById("duration").value)
+    
+    let rP = document.getElementById("resP");
+    let rC = document.getElementById("resC");
+    let rNA = document.getElementById("resNA");
+    
+    let calP = (((adult * proteina) + (kids * proteinaKid)) * duration) / transformUnd;
+    let calC = (adult * beer * duration) / transformUnd;
+    let calNA = (((adult * nonAlcoholic) + (kids * nonAlcoholicKids)) * duration) / transformUnd;
+    
+    rP.innerText = `A quantidade de proteína: ${calP} kg.`
+    rC.innerText = `A quantidade de Cerveja: ${calC} Litros.`
+    rNA.innerText = `A quantidade de Bebidas não alcoólicas: ${calNA} Litros.`
+    document.getElementById("elemento").style.display = "block";
+}
